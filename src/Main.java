50ce5822c7740fa7bc86de6ad67cb8345f842d26
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-16 22:40:08 -0300

    Exercicio que cria funcionários utilizando SuperClasses e subclasses, dia 16/04

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Empregado[] empregado = new Empregado[3];
-        Util util = new Util();
+        new Util().menu();
 
-        empregado[0] = new EmpregadoComissionado(558975, "lu", 100, 10);
-        empregado[1] = new EmpregadoHorista(559201, "roxa", 25, 50);
-        empregado[2] = new EmpregadoHorista(559988, "vito", 30, 60);
-
-        for (Empregado salario : empregado) {
-            System.out.print(salario.getDados());
-            System.out.println(salario.calcularSalario());
-        }
+//        empregado[0] = new EmpregadoComissionado(558975, "lu", 100, 10);
+//        empregado[1] = new EmpregadoHorista(559201, "roxa", 25, 50);
+//        empregado[2] = new EmpregadoHorista(559988, "vito", 30, 60);
+//
+//        for (Empregado salario : empregado) {
+//            System.out.print(salario.getDados());
+//            System.out.println(salario.calcularSalario());
+//        }
 
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -1,32 +1,90 @@
 import javax.swing.*;
 
-import javax.swing.*;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static javax.swing.JOptionPane.*;
 
 public class Util {
-    private Empregado[] empregado = new Empregado[5];
-    private int index = 0;
+    private Controle controle = new Controle();
 
-    public void inserir(Empregado empregado) {
-        this.empregado[index++] = empregado;
+    public void menu() {
 
-    }
+        int opcao;
+        String menu = "1. Cadastrar novo funcionário\n2. Pesquisar por um funcionário\n3. Listar todos os funcionários";
 
-    public Empregado pesquisar(long matricula) {
-        for (int i = 0; i < index; i++) {
-            if (empregado[i].matricula == matricula) {
-                return empregado[i];
+
+
+        while (true) {
+            opcao = parseInt(showInputDialog(null, menu));
+
+            switch (opcao) {
+                case 1:
+                    cadastrar();
+                    break;
+                case 2:
+                    pesquisar();
+                    break;
+                case 3:
+                    listar();
+                    break;
+                case 4:
+                    return; // finaliza o método
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida");
             }
         }
-        return null;
     }
 
-    public String listar() {
-        String aux = "";
-        for (int i = 0; i < index; i++) {
-            aux += empregado[i].getDados();
-            aux += "Salário: " + empregado[i].calcularSalario();
+    private void pesquisar() {
+        long matricula = parseLong(showInputDialog("Informe a matricula à ser pesquisada"));
+        Empregado empregado = controle.pesquisar(matricula);
+
+        if(empregado == null) {
+            showMessageDialog(null, "Empregado não encontrado");
+        } else {
+            showMessageDialog(null, empregado.getDados());
         }
-        return aux;
+    }
+
+    public void cadastrar() {
+        long matricula = 0;
+        String nome = "";
+        int totalDeHorasTrabalhadas;
+        double valorDaHoraTrabalhada;
+        double totalDeVendas, comissao;
+        String aux = "Deseja criar que tipo de empregado?\n1. Horista\n2. Comissionado\n3. Sair";
+
+        while (true) {
+            int opcao = parseInt(showInputDialog(aux));
+
+            if (opcao == 1 || opcao == 2) {
+                matricula = parseLong(showInputDialog("Informe a matrícula do empregado"));
+                nome = showInputDialog("Digite o nome do empregado");
+            } else if (opcao == 3) {
+                return;
+            } else {
+                showMessageDialog(null,"Opção inválida");
+            }
+
+            switch (opcao) {
+                case 1:
+                    totalDeHorasTrabalhadas = parseInt(showInputDialog("Informe o total de horas trabalhadas do funcionario " + nome));
+                    valorDaHoraTrabalhada = parseDouble(showInputDialog("Informe o valor da hora trabalhada"));
+                    controle.inserir(new EmpregadoHorista(matricula, nome, totalDeHorasTrabalhadas, valorDaHoraTrabalhada));
+                    break;
+                case 2:
+                    totalDeVendas = parseDouble(showInputDialog("Informe o total das vendas do funcionario " + nome));
+                    comissao = parseDouble(showInputDialog("Informe a porcentagem de comissao"));
+                    controle.inserir(new EmpregadoComissionado(matricula, nome, totalDeVendas, comissao));
+                    break;
+            }
+
+        }
+
+    }
+
+    public void listar() {
+        showMessageDialog(null, controle.listar());
     }
 }
-
